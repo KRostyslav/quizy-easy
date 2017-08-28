076673917a18bcf507038e639ef4fa89a175871c
krostyslav <rostyslav.kryvyy@gmail.com>
2017-08-28 21:40:39 +0300

    update quiz logic

--- js/controllers/quiz.controller.js
+++ js/controllers/quiz.controller.js
@@ -13,6 +13,54 @@
         vm.quizMetrics = quizMetrics;
         vm.dataService = DataService;
         vm.activeQuestion = 0;
+        var numQuestionsAnswered = 0;
+
+        vm.questionAnswered = questionAnswered;
+        vm.setActiveQuestion = setActiveQuestion;
+        vm.selectAnswer = selectAnswer;
+
+        /////////////////////////////////////
+
+        function setActiveQuestion(index){
+            // if(index === undefined){
+            //     var breakOut = false;
+            //
+            //     var quizLength = DataService.quizQuestions.length - 1;
+            //
+            //     while(!breakOut){
+            //
+            //         if(DataService.quizQuestions[vm.activeQuestion].selected === null){
+            //             breakOut = true;
+            //         }
+            //
+            //     }
+            // }else{
+            //     vm.activeQuestion = index;
+            // }
+            var breakOut = false;
+            var quizLength = DataService.quizQuestions.length - 1;
+
+            while(!breakOut){
+                vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+
+                if(DataService.quizQuestions[vm.activeQuestion].selected === null){
+                    breakOut = true;
+                }
+            }
+        }
+
+        function questionAnswered(){
+
+            if(DataService.quizQuestions[vm.activeQuestion].selected !== null){
+                numQuestionsAnswered++;
+            }
+            vm.setActiveQuestion();
+        }
+
+        function selectAnswer(index){
+            DataService.quizQuestions[vm.activeQuestion].selected = index;
+        }
+
     }
 
 })();
