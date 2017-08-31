345548632343d91498537b3910ba4d65ff741bc8
krostyslav <rostyslav.kryvyy@gmail.com>
2017-08-31 23:34:43 +0300

    update result

--- js/controllers/results.controller.js
+++ js/controllers/results.controller.js
@@ -8,8 +8,48 @@
 
     function ResultsController(quizMetrics, DataService){
         var vm = this;
+
         vm.quizMetrics = quizMetrics;
         vm.dataService = DataService;
+
+        vm.activeQuestion = 0;
+
+        vm.setActiveQuestion = setActiveQuestion;
+        vm.calculatePerc = calculatePerc;
+        vm.getAnswerClass = getAnswerClass;
+        vm.reset = reset;
+
+        //////////////////////////////////////////////////////////////
+
+        function setActiveQuestion(index){
+            vm.activeQuestion = index;
+        }
+
+        function calculatePerc(){
+            return quizMetrics.numCorrect / DataService.quizQuestions.length * 100;
+        }
+
+        function getAnswerClass(index){
+
+            if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
+                return "bg-success";
+            }else if(index === DataService.quizQuestions[vm.activeQuestion].selected){
+                return "bg-danger";
+            }
+
+        }
+
+        function reset(){
+            quizMetrics.changeState("results", false);
+            quizMetrics.numCorrect = 0;
+
+            for(var i = 0; i < DataService.quizQuestions.length; i++){
+                var data = DataService.quizQuestions[i]; //binding the current question to data to keep code clean
+
+                data.selected = null;
+                data.correct = null;
+            }
+        }
     }
 
 })();
